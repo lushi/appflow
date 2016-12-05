0c96bf739df132563c9966054f689ac1dbabfb09
Lushi Li <lushili06@gmail.com>
2016-12-05 17:00:00 -0500

    better way to intergrate express-handlebars

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
 const express = require('express');
 const path = require('path');
-const handlebars = require('express-handlebars');
+const exphbs = require('express-handlebars');
 const app = express();
 require('./router')(app);
 
-app.set('views', path.join(__dirname, 'view'));
+// app.set('views', path.join(__dirname, 'views'));
 
 /* express-handlebars */
-const hbs = handlebars.create({
-  defaultLayour: 'main'
-});
-
-app.engine('handlebars', hbs.engine);
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 const listener = app.listen(3000, function() {

679b9ace3cb887f5e3363292c38604321a4b4bbb
Lushi Li <lushili06@gmail.com>
2016-12-05 22:47:46 -0500

    add multer and body-parser to parse request data

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const path = require('path');
+
 const exphbs = require('express-handlebars');
 const app = express();
-require('./router')(app);
 
-// app.set('views', path.join(__dirname, 'views'));
+/* form parser */
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 /* express-handlebars */
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+
+require('./router')(app);
 
 const listener = app.listen(3000, function() {
   console.log('Listening on ' + listener.address().port + '!');

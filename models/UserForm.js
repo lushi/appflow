d33315798e56d143d243c6b121ec5dc3b1a801f3
Lushi Li <lushili06@gmail.com>
2016-12-07 18:23:46 -0500

    flesh out model UserForm

--- models/UserForm.js
+++ models/UserForm.js
@@ -1,26 +1,59 @@
+const Form = require('./Form.js');
+
 class UserForm {
-  constructor(user_id) {
+  constructor(db, user_id) {
+    this.db = db;
     this.user_id = user_id;
+    this.collection = 'user_form';
   }
 
+  /*  Saves data from the given form for this user and appends form_id to
+   *  forms the user has completed (in table/collection 'user_form');
+   */
   saveData(form_id, data) {
-    this.data = data;
-  }
+    // pass data to
+    new Form(this.db, form_id).saveData(data);
 
-  getFormData(form_id) {
-    const data = {
-      id: form_id,
+    // append form_id to list of forms that the user has completed
+    let userForm;
+    try {
+      userForm = this.db.get(this.collection, this.user_id);
+    } catch (e) {
+      userForm = {
+        id: this.user_id,
+        completed: []
+      };
     }
 
-    return data;
+    userForm.completed.push(form_id);
+    this.db.set(this.collection, userForm);
   }
 
+  /* Gets data for the given form from the 'form' table/collection.
+  * The return data includes form id and an array of key-value pairs,
+  * where the key is the form field name, and the value contains the type of
+  * data
+  */
+  getFormData(form_id) {
+    return new Form(this.db, form_id).get();
+  }
+
+  /* Check if given form_id is in list of completed form ids for the user */
   hasCompletedForm(form_id) {
-    return false;
+    const doneForms = this.getCompletedFormIds();
+    return doneForms.indexOf(form_id) > -1;
   }
 
-  getEditableFormIds() {
-    return [];
+  /* Get list of form ids that the user has completed in table/collection
+   * 'user_form'
+  */
+  getCompletedFormIds() {
+    try {
+      return this.db.get(this.collection, this.user_id).completed;
+    } catch (e) {
+      if (e == 'Error: COLLECTION_NOT_EXIST')
+        return [];
+    }
   }
 }
 
